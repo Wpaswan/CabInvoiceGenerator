08c41cec4b11e4bcc2fc03f35e6de282a0a499fb
Wpaswan <dablumcapaswan@gmail.com>
2021-12-29 09:04:39 -0800

    [I added a cod to calculate fare of rides in cab]

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿create database payroll_service
+show database
+select *from sys.databases;
+use payroll_service
+create table employee_payroll
+(id int identity(1,1) primary key,
+name varchar(50) not null,
+salary money,
+start_date date
+
+)
+insert into employee_payroll values
+('Dablu',
+5000000,
+'2022-01-01'
+),('Ram',30000,'2022-01-01')
